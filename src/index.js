8fef016911c33614d00d93148ebc7afe915bf9fb
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2021-05-21 23:55:36 +0700

    add headers

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 require('dotenv').config();
 aws.connectAWS();
 app.set();
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 route(app);
